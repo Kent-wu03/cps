948f42e6fbd7da435c7612c75ddda71ee54066a1
Kent <kentwu37@Gmail.com>
2025-07-04 13:52:49 +0700

    Create cv_poin_farmbot.lua

---
+++ GL SCRIPT/cv_poin_farmbot.lua
@@ -0,0 +1,11 @@
+delay = 10
+
+
+SendPacket(2,"action|input\ntext|`2SC `bCV POIN TO FARMING BOT `2BY `cROCKYBANDEL")
+Sleep(500)
+LogToConsole("`cDelay = "..delay)
+
+while true do
+SendPacket(2,"action|dialog_return|\ndialog_name|activity_purchase|\noffer|4|")
+Sleep(delay)
+end
