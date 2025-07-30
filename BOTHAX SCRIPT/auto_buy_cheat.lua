8b6de7a9d0939d9fe68958c8ed758daff979315c
Kent <kentwu37@Gmail.com>
2025-07-30 19:30:54 +0700

    Update auto_buy_cheat.lua

--- BOTHAX SCRIPT/auto_buy_cheat.lua
+++ BOTHAX SCRIPT/auto_buy_cheat.lua
@@ -1,6 +1,10 @@
 local howmany = 230 -- 1 cheat = 8 hours (cost 50wl)
 local delay = 100
 
+SendPacket(2,"action|input\ntext|`2SC `bCV POIN TO FARMING BOT `2BY `cROCKYBANDEL")
+Sleep(500)
+LogToConsole("`cDelay = "..delay)
+
 for i = 1, howmany do
 SendPacket(2, "action|dialog_return\ndialog_name|buycheat")
 Sleep(delay)
